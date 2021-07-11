2751da96c0c42e3eb451b12c73505ba83c5b3261
Antonia Mouawad <antoniam@scribd.com>
2021-07-11 19:09:28 -0400

    Add data warehouse project

---
+++ cloud_data_warehouses/etl.py
@@ -0,0 +1,47 @@
+import configparser
+import psycopg2
+from sql_queries import copy_table_queries, insert_table_queries
+
+
+def load_staging_tables(cur, conn):
+    """
+        Extracts data into staging tables
+
+        Parameters:
+        - cur: cursor object that allows Python to execute PostgreSQL commands in a database session
+        - conn: connection to the cluster
+    """
+    for query in copy_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def insert_tables(cur, conn):
+    """
+        Inserts (transformed) data into final tables from staging tables
+        
+        Parameters:
+        - cur: cursor object that allows Python to execute PostgreSQL commands in a database session
+        - conn: connection to the cluster
+    """
+    for query in insert_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    cur = conn.cursor()
+    print("Loading staging tables")
+    load_staging_tables(cur, conn)
+    print("Inserting tables")
+    insert_tables(cur, conn)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
